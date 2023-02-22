df725b4a6cfc855ba27168693f1c228981e979ae
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-02-22 22:15:09 +0530

    day-2

---
+++ Scripts/index.js
@@ -0,0 +1,19 @@
+let slideIndex = 0;
+topOfferSlide();
+
+function topOfferSlide() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(topOfferSlide, 2000); // Change image every 2 seconds
+}
