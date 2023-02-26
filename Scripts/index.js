ca0310ef0fa06f3faa2ba2783930dd4253fa3745
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-02-26 21:16:39 +0530

    day-5

--- Scripts/index.js
+++ Scripts/index.js
@@ -10,17 +10,34 @@ function topOfferSlide() {
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
+  if (slideIndex > slides.length) { slideIndex = 1 }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
   setTimeout(topOfferSlide, 2000); // Change image every 2 seconds
 }
+
+
+let url = "https://63f6414b59c944921f708a51.mockapi.io/login/"
+let username = document.getElementById("username")
+let profile = document.getElementById("profile")
+profile.addEventListener("mouseenter", async (e) => {
+  try {
+    let req = await fetch(url)
+    let res = await req.json()
+    username.innerText = res[res.length - 1].name
+  } catch (error) {
+    console.log(error)
+  }
+})
+profile.addEventListener("mouseout", (e) => {
+  username.innerHTML = ""
+})
 
 // ******************************************************
 
@@ -44,3 +61,4 @@ productContainers.forEach((item, i) => {
 })
 
 // ******************************************************
+
